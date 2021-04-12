c6087cb3af192a40fbaf0aa83800828c37a42bb1
Sharkovisky <whiteaxefun@gmail.com>
2021-04-12 18:13:36 -0400

    Excluir e alterar de disciplina

--- app/controllers/disciplina.py
+++ app/controllers/disciplina.py
@@ -13,3 +13,41 @@ def listar_disciplinas():
         lista = Disciplina.query.all()
 
     return render_template("disciplina_listar.html", lista=lista)
+
+@app.route('/disciplinas/deletar/<disciplina_id>')
+def deletar_disciplina(disciplina_id):
+    disciplina = Disciplina.query.filter_by(id=disciplina_id).first()
+    db.session.delete(disciplina)
+    db.session.commit()
+    return redirect(url_for('listar_disciplinas', msg='deletar'))
+
+@app.route('/disciplinas/alterar/<disciplina_id>)', methods=['GET', 'POST'])
+def alterar_disciplina(disciplina_id):
+    if request.method == 'GET':
+        disciplina = Disciplina.query.filter_by(id=disciplina_id).first()
+        return render_template("disciplina_formulario.html", disciplina=disciplina, tipo='alterar')
+    elif request.method == 'POST':
+        nome = request.form['nome']
+        calculo = request.form['calculo']
+
+        disciplina = Disciplina.query.filter_by(id=disciplina_id).first()
+        disciplina.nome = nome
+        disciplina.calculo = calculo
+
+        db.session.add(disciplina)
+        db.session.commit()
+
+        return redirect(url_for('listar_disciplinas', msg='alterar'))
+
+@app.route('/disciplinas/inserir', methods=['GET', 'POST'])
+def inserir_disciplina():
+    if request.method == 'GET':
+        return render_template('disciplina_formulario.html', tipo='inserir')
+    elif request.method == 'POST':
+        nome = request.form['nome']
+        calculo = request.form['calculo']
+
+        d1 = Disciplina(nome=nome, calculo=calculo)
+        db.session.add(d1)
+        db.session.commit()
+        return redirect(url_for('listar_disciplinas', msg='inserir'))
